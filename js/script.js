54a6906729c95474e02736a992befd1638a3f6a8
Darrell Lee <leedar@gmail.com>
2026-03-04 19:29:27 -0600

    Adding initial code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+	//add your javascrip between these two lines of code
+	function myEventFuntion(){
+		var e4 = document.getElementById('entryinput');
+		e4.value = ": Hello World";
+		alert("Darrell Lee:" + e4.value);
+	}
 
-
-
-
-
+	var d4 = document.getElementById('entrybutton');
+	d4.addEventListener('click', myEventFuntion);
+	d4.addEventListener('click', function() {
+		console.log('I was clicked');
+		d4.style.backgroundColor = '#C00';
+		d4.innerHTML = '<strong>Goodbye</strong>';
+	});
+}
 
 
 window.addEventListener('load', init);
